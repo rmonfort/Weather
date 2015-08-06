e92fabc5a54e8c718e1d1f7fd90c896bb7c335fb
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-08-05 20:24:58 -0400

    Added helper classes in order to consume JSON from Wunderground. Fleshed out the rest of the application logic along the partial view for weather display.

--- Weather/Controllers/HomeController.cs
+++ Weather/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Net;
 using Weather.ViewModels;
 using Newtonsoft.Json;
+using Weather.Helpers;
 
 namespace Weather.Controllers
 {
@@ -21,8 +22,16 @@ namespace Weather.Controllers
         public PartialViewResult FetchWeather(string PostalCode)
         {
             var url = "http://api.wunderground.com/api/b0f1b6600ed6fb14/conditions/q/" + PostalCode + ".json";
+            var weather = JsonUtility.DownloadAndDeserializeJson<Rootobject>(url);
 
-            return PartialView("_Weather");
+            ViewData["Location"] = weather.current_observation.display_location.full;
+            ViewData["Weather"] = weather.current_observation.weather;
+            ViewData["WeatherIconUrl"] = weather.current_observation.icon_url;
+            ViewData["Temperature"] = weather.current_observation.temperature_string;
+            ViewData["Wind"] = weather.current_observation.wind_mph;
+            ViewData["Humidity"] = weather.current_observation.relative_humidity;
+            ViewData["ForecastUrl"] = weather.current_observation.forecast_url;
+            return PartialView("_Weather", weather);
         }
     }
 }
---
+++ Weather/Helpers/JsonUtility.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Weather.Helpers
+{
+    public static class JsonUtility
+    {
+        public static T DownloadAndDeserializeJson<T>(string url) where T : new()
+        {
+            using (var webClient = new WebClient())
+            {
+                var json = string.Empty;
+
+                try
+                {
+                    json = webClient.DownloadString(url);
+                }
+                catch (Exception) 
+                {
+                    // Stop
+                }
+
+                // If Json string is not empty, deserialize it to a class and return its instance 
+                return !string.IsNullOrEmpty(json) ? JsonConvert.DeserializeObject<T>(json) : new T();
+            }
+        }
+
+    }
+}
